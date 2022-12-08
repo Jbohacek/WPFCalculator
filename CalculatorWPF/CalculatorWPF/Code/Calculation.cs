e04b2a131de44565c6a57fb5433deaa3151c95dc
bohacekjakub <bohacekjakub@sssvt.cz>
2022-12-08 15:41:03 +0100

    FInal Commit

---
+++ CalculatorWPF/CalculatorWPF/Code/Calculation.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CalculatorWPF.Code
+{
+    public static class Calculation
+    {
+        private static Label? Lb_Result = MainWindow.ResultShow;
+        private static Label? Lb_LastResult = MainWindow.LastResultShow;
+
+        private static string _OutputText = "0";
+        public static double OutputDouble { get { return Convert.ToDouble(_OutputText); } }
+
+        public static string LastOutputText { set { if(Lb_LastResult !=null)Lb_LastResult.Content = value; } }
+
+
+        public static string OutputText { 
+            get 
+            {
+                return _OutputText;
+            }
+            set
+            {
+                //Kontrola, aby nebyla na zacatku 0
+                if (_OutputText == "0" && value == "00") return;
+
+                // Aby to Prepsalo nulu a nebo dalo cislo za carku
+                if (value != "0") if (_OutputText == "0" && (value != "00" && value != "0," )) if (value.Length > 0) { value = value.Substring(1, 1); }
+                // Kontrola aby nebyli 2 carky
+                if (value.Length > 0) if (value.Substring(value.Length - 1, 1) == "," && _OutputText.Contains(",")) return;
+
+
+                //Kontrola Aby se nepsalo za destinou carku nuly zbytecne - nevim zda to implementovat
+                //if (value.Substring(value.Length - 1, 1) == "0" && _OutputText.Contains(",")) return;
+
+                //Zmena velikosti
+                if (OutputText.Length < 7)
+                { if (Lb_Result != null) Lb_Result.FontSize = 75; }
+                if (OutputText.Length > 7)
+                { if (Lb_Result != null) Lb_Result.FontSize = 50; }
+                if (OutputText.Length > 10)
+                { if (Lb_Result != null) Lb_Result.FontSize = 25; }
+                if (OutputText.Length > 20)
+                { if (Lb_Result != null) Lb_Result.FontSize = 10; }
+                //Zmena Velikosti
+
+                _OutputText = value;
+                if(Lb_Result != null) Lb_Result.Content = _OutputText;
+            }
+        }
+
+        public static void RemoveLastNumber()
+        {
+            if (_OutputText.Length > 0)
+            {
+                OutputText = _OutputText.Substring(0, _OutputText.Length - 1);
+                if (_OutputText.Length == 0)
+                {
+                    OutputText = "0";
+                }
+            }
+        }
+
+        public static void SoftReset()
+        {
+            OutputText = "0";
+        }
+        public static void HardReset()
+        {
+            OutputText = "0";
+            LastOutputText = "";
+            Operace = MatWays.None;
+        }
+
+
+        public static void MathPowerUp(int PowerCislo)
+        {
+            LastOutputText = OutputText;
+            OutputText = Convert.ToString(Math.Pow(OutputDouble, PowerCislo));
+        }
+
+        public static void Square()
+        {
+            LastOutputText = OutputText;
+            OutputText = Convert.ToString(Math.Sqrt(OutputDouble));
+        }
+
+        public static void DevideByX()
+        {
+            LastOutputText = "1 / " + OutputText;
+            OutputText = Convert.ToString(1 / OutputDouble); 
+        }
+
+        public static void Procetage()
+        {
+            if (Operace == MatWays.None) return;
+
+            string promena = OutputText;
+
+            if (promena.Contains(","))
+            {
+                promena = promena.Insert(promena.IndexOf(",") - 2, ",");
+                promena = promena.Remove(promena.IndexOf(",", promena.IndexOf(",") + 1), 1);
+                if (promena.Length == 5) promena = "0" + promena;
+            }
+            else
+            {
+                promena = promena.Insert(promena.Length - 2, ",");
+                if (promena.Length == 3) promena = "0" + promena;
+            }
+            OutputText = promena;
+        }
+
+
+
+
+        private static MatWays Operace = MatWays.None;
+        private static double PrveCislo = 0;
+
+        public static void Operuj(MatWays OperaceZadana)
+        {
+            
+            PrveCislo = OutputDouble;
+            
+            char posledniZnamenko = '+';
+            switch (OperaceZadana)
+            {
+                case MatWays.Plus:
+                    posledniZnamenko = '+';
+                    break;
+                case MatWays.Minus:
+                    posledniZnamenko = '-';
+                    break;
+                case MatWays.Devide:
+                    posledniZnamenko = '÷';
+                    break;
+                case MatWays.Multiply:
+                    posledniZnamenko = '×';
+                    break;
+            }
+            LastOutputText = PrveCislo.ToString() + " " + posledniZnamenko;
+            SoftReset();
+            Operace = OperaceZadana;
+        }
+
+        public static void Calculate()
+        {
+            if (Operace == MatWays.None)
+            {
+                return;
+            }
+            double vysledek = 0;
+            switch (Operace)
+            {
+                case MatWays.None:
+                    break;
+                case MatWays.Plus:
+                    vysledek = OutputDouble + PrveCislo;
+                    break;
+                case MatWays.Minus:
+                    vysledek = PrveCislo - OutputDouble;
+                    break;
+                case MatWays.Devide:
+                    if (OutputDouble == 0) { MessageBox.Show("Nemůžeš dělit nulou", "Chyba", MessageBoxButton.OK, MessageBoxImage.Error);break; }
+                    vysledek = PrveCislo / OutputDouble;
+                    break;
+                case MatWays.Multiply:
+                    vysledek = PrveCislo * OutputDouble;
+                    break;
+
+            }
+            Operace = MatWays.None;
+            if (Lb_LastResult != null) LastOutputText = Lb_LastResult.Content + " " + OutputText;
+
+            OutputText = vysledek.ToString();
+        }
+
+        
+    }
+}
---
+++ CalculatorWPF/CalculatorWPF/MainWindow.xaml.cs
@@ -0,0 +1,244 @@
+﻿using CalculatorWPF.Code;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace CalculatorWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Label? LastResultShow;
+        public static Label? ResultShow;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ResultShow = Lb_Result;
+            LastResultShow = Lb_LastResult;
+        }
+
+        private void Bt_Zero_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.OutputText += "0";
+        }
+
+        private void Bt_One_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.OutputText += "1";
+        }
+
+        private void Bt_Two_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.OutputText += "2";
+        }
+
+        private void Bt_Three_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.OutputText += "3";
+        }
+
+        private void Bt_Four_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.OutputText += "4";
+        }
+
+        private void Bt_Five_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.OutputText += "5";
+        }
+
+        private void Bt_Six_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.OutputText += "6";
+        }
+
+        private void Bt_Seven_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.OutputText += "7";
+        }
+
+        private void Bt_Eight_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.OutputText += "8";
+        }
+
+        private void Bt_Nine_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.OutputText += "9";
+        }
+
+        private void Bt_Backspace_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.RemoveLastNumber();
+        }
+
+        private void Bt_C_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.SoftReset();
+        }
+        private void Bt_CE_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.HardReset();
+        }
+
+        private void Bt_Comma_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.OutputText += ",";
+        }
+
+        private void Bt_Power_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.MathPowerUp(2);
+            Lb_LastResult.Content += "²";
+        }
+
+        
+
+        private void Bt_Square_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.Square();
+            Lb_LastResult.Content = "√" + Lb_LastResult.Content;
+        }
+
+        private void Bt_Plus_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.Operuj(MatWays.Plus);
+        }
+
+        private void Bt_Minus_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.Operuj(MatWays.Minus);
+        }
+
+        private void Bt_Multi_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.Operuj(MatWays.Multiply);
+        }
+
+        private void Bt_Devide_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.Operuj(MatWays.Devide);
+        }
+
+        private void Bt_GetResult_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.Calculate();
+        }
+
+        private void Bt_OneDevidedX_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.DevideByX();
+        }
+
+        private void Bt_Precentage_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation.Procetage();
+        }
+
+        private void DockPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void MinimazedButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        //private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (this.WindowState == WindowState.Maximized)
+        //    {
+        //        WindowState = WindowState.Normal;
+        //    }
+        //    else
+        //    {
+        //        this.WindowState = WindowState.Maximized;
+        //    }
+            
+        //}
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.NumPad0:
+                    Calculation.OutputText += "0";
+                    break;
+                case Key.NumPad1:
+                    Calculation.OutputText += "1";
+                    break;
+                case Key.NumPad2:
+                    Calculation.OutputText += "2";
+                    break;
+                case Key.NumPad3:
+                    Calculation.OutputText += "3";
+                    break;
+                case Key.NumPad4:
+                    Calculation.OutputText += "4";
+                    break;
+                case Key.NumPad5:
+                    Calculation.OutputText += "5";
+                    break;
+                case Key.NumPad6:
+                    Calculation.OutputText += "6";
+                    break;
+                case Key.NumPad7:
+                    Calculation.OutputText += "7";
+                    break;
+                case Key.NumPad8:
+                    Calculation.OutputText += "8";
+                    break;
+                case Key.NumPad9:
+                    Calculation.OutputText += "9";
+                    break;
+                case Key.Multiply:
+                    Calculation.Operuj(MatWays.Multiply);
+                    break;
+                case Key.Subtract:
+                    Calculation.Operuj(MatWays.Minus);
+                    break;
+                case Key.Add:
+                    Calculation.Operuj(MatWays.Plus);
+                    break;
+                case Key.Divide:
+                    Calculation.Operuj(MatWays.Devide);
+                    break;
+                case Key.Decimal:
+                    Calculation.OutputText += ",";
+                    break;
+                case Key.Enter:
+                    Calculation.Calculate();
+                    break;
+                case Key.Escape:
+                    Calculation.SoftReset();
+                    break;
+                case Key.Back:
+                    Calculation.RemoveLastNumber();
+                    break;
+            }
+        }
+    }
+}
